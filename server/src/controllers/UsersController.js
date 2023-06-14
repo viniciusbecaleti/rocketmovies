1196a0ee274c6d29a9ff3654c761a61c4c25fd35
Vinicius Becaleti <vinicius@becaleti.com>
2023-06-14 03:33:03 -0300

    feat: add routes and users controller

--- server/src/controllers/UsersController.js
+++ server/src/controllers/UsersController.js
@@ -1,8 +1,143 @@
 const AppError = require("../utils/AppError")
+const { hash, compare } = require("bcrypt")
+const validateEmail = require("../utils/validateEmail")
+const knex = require("../database/knex")
+const { v4: uuidv4 } = require("uuid")
 
 class UsersController {
   async index(req, res) {
-    res.json({ message: "Hello, world!" })
+    const users = await knex("users").select("*").orderBy("created_at", "desc")
+
+    return res.json(users)
+  }
+
+  async show(req, res) {
+    const { user_id } = req.params
+
+    const user = await knex("users").select("*").where({ id: user_id }).first()
+
+    if (!user) {
+      throw new AppError("User not found")
+    }
+
+    return res.json(user)
+  }
+
+  async create(req, res) {
+    const { name, email, password, confirmPassword } = req.body
+
+    if (!name) {
+      throw new AppError("Please enter a name")
+    }
+
+    if (!email) {
+      throw new AppError("Please enter an email")
+    }
+
+    if (!password) {
+      throw new AppError("Please enter a password")
+    }
+
+    if (!confirmPassword) {
+      throw new AppError("Please enter a password confirmation")
+    }
+
+    const isValidEmail = validateEmail(email)
+
+    if (!isValidEmail) {
+      throw new AppError("Please enter a valid email")
+    }
+
+    const user = await knex("users").select("*").where({ email }).first()
+
+    if (user) {
+      throw new AppError("User already exists")
+    }
+
+    if (password !== confirmPassword) {
+      throw new AppError("Passwords do not match")
+    }
+
+    const id = uuidv4()
+    const hashedPassword = await hash(password, 10)
+
+    await knex("users").insert({ id, name, email, password: hashedPassword })
+
+    return res.sendStatus(201)
+  }
+
+  async update(req, res) {
+    const { user_id } = req.params
+
+    const user = await knex("users").select("*").where({ id: user_id }).first()
+
+    if (!user) {
+      throw new AppError("User not found")
+    }
+
+    const { name, email, oldPassword, newPassword, confirmNewPassword } = req.body
+
+    if (name) {
+      user.name = name
+    }
+
+    if (email) {
+      const isValidEmail = validateEmail(email)
+
+      if (!isValidEmail) {
+        throw new AppError("Please enter a valid email")
+      }
+
+      const emailAlreadyAssigned = await knex("users").select("*").where({ email }).first()
+
+      if (emailAlreadyAssigned) {
+        throw new AppError("Email already assigned")
+      }
+
+      user.email = email
+    }
+
+    if (oldPassword || newPassword || confirmNewPassword) {
+      if (!oldPassword || !newPassword || !confirmNewPassword) {
+        throw new AppError("Please enter a old password, new password and confirm new password to change your password")
+      }
+
+      const isValidOldPassword = await compare(oldPassword, user.password)
+
+      if (!isValidOldPassword) {
+        throw new AppError("Old password is incorrect")
+      }
+
+      if (newPassword !== confirmNewPassword) {
+        throw new AppError("New passwords do not match")
+      }
+
+      const hashedPassword = await hash(newPassword, 10)
+
+      user.password = hashedPassword
+    }
+
+    await knex("users").update(user).where({ id: user_id })
+
+    return res.sendStatus(200)
+  }
+
+  async delete(req, res) {
+    const { user_id } = req.params
+
+    if (!user_id) {
+      throw new AppError("Please enter a user_id")
+    }
+
+    const user = await knex("users").select("*").where({ id: user_id }).first()
+
+    if (!user) {
+      throw new AppError("User not found")
+    }
+
+    await knex("users").where({ id: user_id }).del()
+
+    return res.sendStatus(204)
   }
 }
 
--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -5,5 +5,9 @@ const usersRoutes = Router()
 const usersController = new UsersController()
 
 usersRoutes.get("/", usersController.index)
+usersRoutes.get("/:user_id", usersController.show)
+usersRoutes.post("/", usersController.create)
+usersRoutes.put("/:user_id", usersController.update)
+usersRoutes.delete("/:user_id", usersController.delete)
 
 module.exports = usersRoutes
