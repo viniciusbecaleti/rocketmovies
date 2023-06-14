bde47b1b145be94694f3dff5ca4b972cd1cd8403
Vinicius Becaleti <vinicius@becaleti.com>
2023-06-14 11:46:33 -0300

    fix: Fix uuid column which is primary but need to specify notNullable

--- server/src/database/knex/migrations/20230614045702_createUsers.js
+++ server/src/database/knex/migrations/20230614045702_createUsers.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
   return knex.schema.createTable("users", table => {
-    table.uuid("id").primary()
+    table.uuid("id").primary().notNullable()
     table.string("name").notNullable()
     table.string("email").notNullable()
     table.string("password").notNullable()
