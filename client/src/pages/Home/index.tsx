5a2a91a8e1aa7f5c6d3d8a445e18109664c3659a
Vinicius Becaleti <vinicius@becaleti.com>
2023-07-10 22:30:34 -0300

    refactor: refactor Home page

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,37 +1,54 @@
+import { useEffect, useRef, useState } from 'react'
 import { FiPlus } from 'react-icons/fi'
 
-import { HomeContainer, HomeMain, MovieList, Movies } from './styles'
+import { HomeContainer, HomeMain, MovieList } from './styles'
 
 import { useWindowSize } from '../../hooks/useWindowSize'
 
 import { Header } from '../../components/Header'
-import { Button } from '../../components/Button'
 import { Movie } from '../../components/Movie'
+import { ButtonLink } from '../../components/ButtonLink'
 
 export function Home() {
+  const movieListRef = useRef<HTMLDivElement>(null)
+  const [isScrollMovieListVisible, setIsScrollMovieListVisible] =
+    useState(false)
+
   const { innerWidth } = useWindowSize()
+
+  useEffect(() => {
+    if (movieListRef.current) {
+      const { scrollHeight, offsetHeight } = movieListRef.current
+      setIsScrollMovieListVisible(scrollHeight > offsetHeight)
+    }
+  }, [movieListRef])
 
   return (
     <HomeContainer>
       <Header />
 
       <HomeMain>
-        <Movies>
-          <header>
-            <h1>Meus filmes</h1>
-            <Button small>
-              <FiPlus />
-              {innerWidth > 480 && 'Adicionar filme'}
-            </Button>
-          </header>
+        <div>
+          <h1>Meus filmes</h1>
+          <ButtonLink to="/create">
+            <FiPlus />
+            {innerWidth > 480 && 'Adicionar filme'}
+          </ButtonLink>
+        </div>
 
-          <MovieList>
-            <Movie />
-            <Movie />
-            <Movie />
-            <Movie />
-          </MovieList>
-        </Movies>
+        <MovieList
+          ref={movieListRef}
+          isScrollMovieListVisible={isScrollMovieListVisible}
+        >
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+        </MovieList>
       </HomeMain>
     </HomeContainer>
   )
--- client/src/pages/Home/styles.ts
+++ client/src/pages/Home/styles.ts
@@ -4,10 +4,16 @@ export const HomeContainer = styled.div`
   height: 100vh;
 
   display: grid;
-  grid-template-rows: auto 1fr;
+  grid-template-rows: 116rem calc(100vh - 116rem);
   grid-template-areas:
     'header'
     'content';
+
+  overflow: hidden;
+
+  @media (max-width: 480px) {
+    grid-template-rows: 80rem calc(100vh - 80rem);
+  }
 `
 
 export const HomeMain = styled.main`
@@ -16,35 +22,46 @@ export const HomeMain = styled.main`
   width: 100%;
   max-width: 1152rem;
   margin-inline: auto;
-  margin-top: 116rem;
-  padding: 40rem 16rem;
+  margin-bottom: 30rem;
+  padding-inline: 16rem;
 
-  @media (max-width: 480px) {
-    margin-top: 86rem;
-  }
-`
+  display: grid;
+  grid-template-rows: 132rem calc(100% - 132rem);
 
-export const Movies = styled.section`
-  header {
+  overflow: hidden;
+
+  > div:first-child {
     display: flex;
     align-items: center;
     justify-content: space-between;
 
-    h1 {
-      font-size: 32rem;
-      font-weight: 400;
-    }
-
-    button {
+    a {
       width: fit-content;
     }
   }
+
+  @media (max-width: 480px) {
+    grid-template-rows: 80rem calc(100% - 80rem);
+  }
 `
 
-export const MovieList = styled.div`
-  margin-top: 32rem;
+interface MovieListProps {
+  isScrollMovieListVisible: boolean
+}
+
+export const MovieList = styled.div<MovieListProps>`
+  justify-items: start;
 
   display: flex;
   flex-direction: column;
   gap: 24rem;
+
+  overflow-y: auto;
+
+  padding-right: ${({ isScrollMovieListVisible }) =>
+    isScrollMovieListVisible && '16rem'};
+
+  @media (max-width: 480px) {
+    gap: 16rem;
+  }
 `
